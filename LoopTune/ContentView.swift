5221e4e8d1a8d72927485917e1413d4acf575303
Jordan Moreno <jordam4@uci.edu>
2024-12-21 21:05:33 -0800

    Initial Commit

---
+++ LoopTune/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoopTune
+//
+//  Created by Jordan Moreno on 12/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoopTune/LoopTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopTuneApp.swift
+//  LoopTune
+//
+//  Created by Jordan Moreno on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LoopTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
